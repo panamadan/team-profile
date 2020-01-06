8f0739228f0de98eab97a43b26cb36ba6dc1444b
Juan De La Guardia <delaguardia@outlook.com>
2020-01-06 16:08:29 -0500

    new

---
+++ DevelopOption2/htmlRender/mainRender.js
@@ -0,0 +1,9 @@
+
+// create a function that holds the html that will output the main body of the html and will take in the data from the other cards
+function mainRender(data){
+    return `
+// header and body for html
+`
+}
+// export mainRender
+module.exports = mainRender
---
+++ DevelopOption2/lib/Manager.js
@@ -0,0 +1,12 @@
+// require Employee class
+const Employee = require("./Employee");
+
+// create a class called manager that extends Employee
+class Manager extends Employee{
+    constructor(name, id, email, officeNumber){
+        super()
+        this.Manager
+    }
+}
+// export manager
+module.exports = Manager
